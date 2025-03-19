0a0f2fb41093d44c23ea85f71ab84802f9ae830c
GauravHub11 <pandeya.gaurav@gmail.com>
2025-03-19 23:56:27 +0545

    update configuration

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # API Key for Gemini
-GEMINI_API_KEY = os.getenv("GOOGLE_API_KEY")
+API_KEY = os.getenv("API_KEY")
 
-if not GEMINI_API_KEY:
+if not API_KEY:
     raise ValueError("Missing GOOGLE_API_KEY. Please set it in .env")
